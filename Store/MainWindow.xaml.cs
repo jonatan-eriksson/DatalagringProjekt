efcade33f63a27437109037f22e2aeb0eedd37a1
Sandra <sandralindberg91@hotmail.com>
2020-11-23 16:17:54 +0100

    Fixade lite med mainwindow

--- Store/MainWindow.xaml.cs
+++ Store/MainWindow.xaml.cs
@@ -23,6 +23,57 @@ namespace Store
         public MainWindow()
         {
             InitializeComponent();
+
+            State.Movies = API.GetMovieSlice(0, 30);
+            for (int y = 0; y < MovieGrid.RowDefinitions.Count; y++)
+            {
+                for (int x = 0; x < MovieGrid.ColumnDefinitions.Count; x++)
+                {
+                    int i = y * MovieGrid.ColumnDefinitions.Count + x;
+                    if (i < State.Movies.Count)
+                    {
+                        var movie = State.Movies[i];
+
+                        try
+                        {
+                            var image = new Image() { };
+                            image.Cursor = Cursors.Hand;
+                            image.MouseUp += Image_MouseUp;
+                            image.HorizontalAlignment = HorizontalAlignment.Center;
+                            image.VerticalAlignment = VerticalAlignment.Center;
+                            image.Source = new BitmapImage(new Uri(movie.ImageURL));
+                            //image.Height = 120;
+                            image.Margin = new Thickness(4, 4, 4, 4);
+
+                            MovieGrid.Children.Add(image);
+                            Grid.SetRow(image, y);
+                            Grid.SetColumn(image, x);
+                        }
+                        catch (Exception e) when
+                            (e is ArgumentNullException ||
+                             e is System.IO.FileNotFoundException ||
+                             e is UriFormatException)
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
         }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var x = Grid.GetColumn(sender as UIElement);
+            var y = Grid.GetRow(sender as UIElement);
+
+            int i = y * MovieGrid.ColumnDefinitions.Count + x;
+            State.Pick = State.Movies[i];
+
+            if (API.RegisterSale(State.User, State.Pick))
+                MessageBox.Show("All is well and you can download your movie now.", "Sale Succeeded!", MessageBoxButton.OK, MessageBoxImage.Information);
+            else
+                MessageBox.Show("An error happened while buying the movie, please try again at a later time.", "Sale Failed!", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+    }
     }
 }
