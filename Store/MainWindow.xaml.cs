4872e4fccbde74401543b663d0c21d6e075ea03e
Sandra <sandralindberg91@hotmail.com>
2020-11-26 16:14:25 +0100

    Försökte lägga till en Grid i Main så att man kan byta sida

--- Store/MainWindow.xaml.cs
+++ Store/MainWindow.xaml.cs
@@ -28,6 +28,7 @@ namespace Store
             InitializeComponent();
           
         }
+        
         private void HamburgerMenuControl_OnItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs e)
         {
             this.HamburgerMenuControl.Content = e.InvokedItem;
@@ -37,7 +38,57 @@ namespace Store
                 // close the menu if a item was selected
                 // this.HamburgerMenuControl.IsPaneOpen = false;
             }
+
+            
         }
+        private void GoFilmer_Click(object sender, RoutedEventArgs e)
+        {
+            Title.Content = "Filmer";
+            Filmer.Visibility = Visibility.Visible;
+            Konto.Visibility = Visibility.Hidden;
+            MinaFilmer.Visibility = Visibility.Hidden;
+            Om.Visibility = Visibility.Hidden;
+            Inställningar.Visibility = Visibility.Hidden;
+        }
+
+        private void GoKonto_Click(object sender, RoutedEventArgs e)
+        {
+            Title.Content = "Konto";
+            Filmer.Visibility = Visibility.Hidden;
+            Konto.Visibility = Visibility.Visible;
+            MinaFilmer.Visibility = Visibility.Hidden;
+            Om.Visibility = Visibility.Hidden;
+            Inställningar.Visibility = Visibility.Hidden;
+        }
+
+        private void GoMinaFilmer_Click(object sender, RoutedEventArgs e)
+        {
+            Title.Content = "Mina Filmer";
+            Filmer.Visibility = Visibility.Hidden;
+            Konto.Visibility = Visibility.Hidden;
+            MinaFilmer.Visibility = Visibility.Visible;
+            Om.Visibility = Visibility.Hidden;
+            Inställningar.Visibility = Visibility.Hidden;
+        }
+        private void GoOm_Click(object sender, RoutedEventArgs e)
+        {
+            Title.Content = "Om";
+            Filmer.Visibility = Visibility.Hidden;
+            Konto.Visibility = Visibility.Hidden;
+            MinaFilmer.Visibility = Visibility.Hidden;
+            Om.Visibility = Visibility.Visible;
+            Inställningar.Visibility = Visibility.Hidden;
+        }
+        private void GoInställningar_Click(object sender, RoutedEventArgs e)
+        {
+            Title.Content = "Store";
+            Filmer.Visibility = Visibility.Visible;
+            Konto.Visibility = Visibility.Hidden;
+            MinaFilmer.Visibility = Visibility.Hidden;
+            Om.Visibility = Visibility.Hidden;
+            Inställningar.Visibility = Visibility.Visible;
+        }
+        
 
     }
 }
