5f65f05c5591dfc19d3671ad2d7a6ad427a1f23c
Jonatan Eriksson <jonatan-eriksson@users.noreply.github.com>
2020-12-02 15:07:23 +0100

    Update EF models

--- Database/Model.cs
+++ Database/Model.cs
@@ -8,13 +8,15 @@ namespace Database
     public class Customer
     {
         public int Id { get; set; }
-        // Unique username
         [Required]
         public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        
-        public List<Sale> Sales { get; set; }
+        public DateTime RegisterDate { get; set; }
+
+        public virtual List<Sale> Sales { get; set; }
     }
 
     public class Movie
@@ -22,11 +24,12 @@ namespace Database
         public int Id { get; set; }
         public string Title { get; set; }
         public string Year { get; set; }
+        public string Rating { get; set; }
         public string ImageUrl { get; set; }
         public decimal Price { get; set; }
 
-        public List<Genre> Genres { get; set; }
-        public List<Sale> Sales { get; set; }
+        public virtual List<Genre> Genres { get; set; }
+        public virtual List<Sale> Sales { get; set; }
         
     }
 
@@ -34,7 +37,7 @@ namespace Database
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Movie> Movies { get; set; }
+        public virtual List<Movie> Movies { get; set; }
     }
 
     public class Sale
@@ -42,7 +45,9 @@ namespace Database
         public int Id { get; set; }
         public DateTime Date { get; set; }
 
-        public Customer Customer { get; set; }
-        public Movie Movie { get; set; }
+        public virtual int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual int MovieId { get; set; }
+        public virtual Movie Movie { get; set; }
     }
 }
